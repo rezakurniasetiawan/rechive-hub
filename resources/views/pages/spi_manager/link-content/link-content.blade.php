295d9ccd027132d734d7225605867dbcbb503864
rezakurniasetiawan <rezakurniasetiawan@gmail.com>
2025-11-20 20:47:18 +0700

    update

--- resources/views/pages/spi_manager/link-content/link-content.blade.php
+++ resources/views/pages/spi_manager/link-content/link-content.blade.php
@@ -203,3 +203,13 @@
     </div>
 </div>
 <!-- END: Data List -->
+
+<script>
+    function copyToClipboard(text) {
+        navigator.clipboard.writeText(text).then(function() {
+            console.log('Text copied to clipboard');
+        }, function(err) {
+            console.error('Could not copy text: ', err);
+        });
+    }
+</script>
